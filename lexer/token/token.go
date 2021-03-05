8324a633456fe92fa56efa7f2e791c8893b64ae7
theBashShell <edohbruno@gmail.com>
2021-03-05 22:15:45 +0000

    lexer + tokens

--- lexer/token/token.go
+++ lexer/token/token.go
@@ -1,11 +1,40 @@
 package token
 
-// Type is a token type
-type Type string 
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	
+	// Identifiers + literals
+	
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+	
+	// Operators
+	
+	ASSIGN = "="
+	PLUS   = "+"
+	
+	// Delimiters
 
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	
+	
+	// Keywords
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+// Type is a token type
+type Type string
 
 // Token represents a token
 type Token struct {
-	Kind Token
-	Literal string	
+	Kind    Type
+	Literal string
 }
