b70c7d02c361c0a4dc939d9e0a9bd15ac6ad67e6
theBashShell <edohbruno@gmail.com>
2021-03-30 22:25:16 +0000

    added tostring to node interface

--- ast/ast.go
+++ ast/ast.go
@@ -1,12 +1,14 @@
 package ast
 
 import (
+	"bytes"
+
 	"github.com/thebashshell/compilers/token"
 )
 
 type Node interface {
 	TokenLiteral() string
-	// String() string
+	String() string
 }
 
 type Expression interface {
@@ -32,7 +34,11 @@ func (p *Program) TokenLiteral() string {
 }
 
 func (p *Program) String() string {
-	return "program"
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
 }
 
 type Identifier struct {
@@ -40,10 +46,9 @@ type Identifier struct {
 	Value string
 }
 
-func (i *Identifier) expressionNode() {}
-
-// func (i *Identifier) String() string       { return "identifier" }
+func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
 
 type LetStatment struct {
 	Token    token.Token
@@ -53,7 +58,17 @@ type LetStatment struct {
 
 func (l *LetStatment) statementNode() {}
 
-// func (l *LetStatment) String() string       { return fmt.Sprintf("%s %s", l.Token, l.Variable) }
+func (l *LetStatment) String() string {
+	var out bytes.Buffer
+	out.WriteString(l.TokenLiteral() + " ")
+	out.WriteString(l.Variable.String())
+	out.WriteString(" = ")
+	if l.Value != nil {
+		out.WriteString(l.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
 func (l *LetStatment) TokenLiteral() string { return l.Token.Literal }
 
 type ReturnStatement struct {
@@ -63,11 +78,26 @@ type ReturnStatement struct {
 
 func (r *ReturnStatement) statementNode()       {}
 func (r *ReturnStatement) TokenLiteral() string { return r.Token.Literal }
-
-type ExpressionStatement struct {
-	Token token.Token
-	Value Expression
+func (r *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(r.TokenLiteral() + " ")
+	if r.Value != nil {
+		out.WriteString(r.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
 }
 
-func (r *ExpressionStatement) statementNode()       {}
-func (r *ExpressionStatement) TokenLiteral() string { return r.Token.Literal }
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (e *ExpressionStatement) statementNode()       {}
+func (e *ExpressionStatement) TokenLiteral() string { return e.Token.Literal }
+func (e *ExpressionStatement) String() string {
+	if e.Expression != nil {
+		return e.Expression.String()
+	}
+	return ""
+}
