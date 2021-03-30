ff8343b7a42467f9ee55eb2b6d1ea7b27bb04893
theBashShell <edohbruno@gmail.com>
2021-03-30 16:00:07 +0000

    repl functional

--- repl/repl.go
+++ repl/repl.go
@@ -1,9 +1,15 @@
 package repl
 
-import "io"
+import (
+	"bufio"
+	"io"
+
+	"github.com/thebashshell/compilers/lexer"
+)
 
 func StartREPL(in io.Reader, out io.Writer) {
-	const PROMPT = `>>>`
+	const PROMPT = `>>> `
+	const WELCOME = `Welcome to IntLang`
 	const GREETING = `
 	
 	██▓ ███▄    █ ▄▄▄█████▓ ██▓    ▄▄▄       ███▄    █   ▄████ 
@@ -20,4 +26,21 @@ func StartREPL(in io.Reader, out io.Writer) {
 	`
 
 	out.Write([]byte(GREETING))
+	out.Write([]byte(WELCOME))
+	out.Write([]byte("\n"))
+
+	for {
+
+		// print prompt
+		out.Write([]byte(PROMPT))
+
+		// get input
+		scanner := bufio.NewScanner(in)
+
+		if scanner.Scan() {
+			l := lexer.New(scanner.Text())
+			out.Write([]byte(l.NextToken().String()))
+		}
+	}
+
 }
