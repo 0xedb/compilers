aa2c77a2c80ac9548e47bdb9270f400d68d18792
theBashShell <edohbruno@gmail.com>
2021-03-30 16:23:04 +0000

    let statment + identifiers

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,9 @@
 package ast
 
+import (
+	"github.com/thebashshell/compilers/token"
+)
+
 type Node interface {
 	TokenLiteral() string
 	String() string
@@ -26,3 +30,26 @@ func (p *Program) TokenLiteral() string {
 
 	return ""
 }
+
+func (p *Program) String() string {
+	return "program"
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) String() string       { return "identifier" }
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+type LetStatment struct {
+	Token    token.Token
+	Variable *Identifier
+	Value    Expression
+}
+
+func (l *LetStatment) statementNode()       {}
+func (l *LetStatment) String() string       { return "letstatement" }
+func (l *LetStatment) TokenLiteral() string { return l.Token.Literal }
