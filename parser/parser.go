064bf2179ba47bdaa42621e530661d1b0042ff08
theBashShell <edohbruno@gmail.com>
2021-03-30 16:35:06 +0000

    program parsing

--- parser/parser.go
+++ parser/parser.go
@@ -1,2 +1,47 @@
 package parser
 
+import (
+	"github.com/thebashshell/compilers/ast"
+	"github.com/thebashshell/compilers/lexer"
+	"github.com/thebashshell/compilers/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	cur, peek token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.cur = p.peek
+	p.peek = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+
+	program.Statements = []ast.Statement{}
+
+	for p.cur.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	return nil
+}
