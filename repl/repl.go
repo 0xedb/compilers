a31c01c2af89255a3c9f4b5e1fa0ac249fe05a94
theBashShell <edohbruno@gmail.com>
2021-04-02 17:29:19 +0000

    repl with parser

--- repl/repl.go
+++ repl/repl.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"io"
 
-	"github.com/0xedb/compilers/token"
+	"github.com/0xedb/compilers/parser"
 
 	"github.com/0xedb/compilers/lexer"
 )
@@ -41,11 +41,28 @@ func StartREPL(in io.Reader, out io.Writer) {
 
 		if scanner.Scan() {
 			l := lexer.New(scanner.Text())
+			p := parser.New(l)
 
-			for output := l.NextToken(); output.Type != token.EOF; output = l.NextToken() {
-				out.Write([]byte(output.String()))
+			pg := p.ParseProgram()
+
+			if len(p.Errors()) != 0 {
+				printParserErrors(out, p.Errors())
+				continue
 			}
+
+			// for output := l.NextToken(); output.Type != token.EOF; output = l.NextToken() {
+			// 	out.Write([]byte(output.String()))
+			// }
+
+			io.WriteString(out, pg.String())
+			io.WriteString(out, "\n")
 		}
 	}
 
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
 }
